b4aef88c4f8ea1070b2fd80f9361acebcd9a561a
Arnab Dey Sarkar <a.d.sarkar1990@gmail.com>
2022-09-05 10:24:10 +0530

    Updated Project

--- src/main/java/com/nextgen/movieAvailabilityAtTheatre/MovieAvailabilityAtTheatreApplication.java
+++ src/main/java/com/nextgen/movieAvailabilityAtTheatre/MovieAvailabilityAtTheatreApplication.java
@@ -4,15 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableWebSecurity
 @Configuration
 @ComponentScan
-public class MovieAvailabilityAtTheatreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MovieAvailabilityAtTheatreApplication.class, args);
-	}
+public class MovieAvailabilityAtTheatreApplication extends WebSecurityConfigurerAdapter {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAvailabilityAtTheatreApplication.class, args);
+    }
 
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
 }
